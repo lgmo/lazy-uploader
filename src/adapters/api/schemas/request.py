b78daafcb6ff02b717c79e7706a58131019c2831
Leonardo Moreira <lgmo@cin.ufpe.br>
2025-07-26 18:07:45 -0300

    feat: add basic schema, entity and config

---
+++ src/adapters/api/schemas/request.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import os
+from uuid import uuid4
+
+from pydantic import BaseModel, HttpUrl
+
+from config import get_settings
+from domain.entities import File, FileStatus
+
+
+class FileCreateSchema(BaseModel):
+    name: str
+    content: bytes
+
+    def to_entity(self) -> File:
+        now = datetime.now()
+        settings = get_settings()
+        final_name, extension = os.path.splitext(self.name)
+        file_id = uuid4()
+        url = HttpUrl(
+            f"{settings.storage_base_url}/{final_name}_{file_id.hex}{extension}"
+        )
+        return File(
+            id=file_id,
+            status=FileStatus.PENDING,
+            url=url,
+            created_at=now,
+            updated_at=now,
+            **self.model_dump(exclude={"name"}),
+        )
---
+++ src/domain/entities.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from enum import StrEnum
+
+from pydantic import BaseModel, HttpUrl
+from pydantic import UUID4
+
+
+class FileStatus(StrEnum):
+    PENDING = "pending"
+    PROCESSING = "processing"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+
+class File(BaseModel):
+    id: UUID4
+    name: str
+    url: HttpUrl
+    status: FileStatus
+    created_at: datetime
+    updated_at: datetime
